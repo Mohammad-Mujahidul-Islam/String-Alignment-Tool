3f0df877c2de6ca47436ce8fe1979cbf4d3a8fd8
Mujahid <mujahid4110@gmail.com>
2025-11-13 00:43:21 +0600

    Refactor alignment logic and password handling
    Refactored password checking and history saving functions. Improved alignment algorithm and added gap/mismatch penalties.

--- Source.cpp
+++ Source.cpp
@@ -4,38 +4,61 @@ using namespace std;
 const string PASSWORD_FILE = "password.txt";
 const string HISTORY_FILE  = "history.txt";
 
-// ------------------- Utility Functions -------------------
-
-string readPasswordFromFile() {
-    ifstream file(PASSWORD_FILE);
+// Check Admin Password
+bool checkPassword()
+{
+    ifstream file(PASSWORD_FILE); // Open password file
     string password;
-    if (file.is_open()) {
-        getline(file, password);
+
+    if (!file)
+    {
+        cout << "Password file missing or empty!\n";
+        return false;
+    }
+    else
+    {
+        getline(file, password); // Read password from file
         file.close();
     }
-    return password;
+
+    string input;
+    cout << "Enter Admin Password: ";
+    cin >> input;
+
+    if (input == password)
+    {
+        cout << "Access Granted!\n";
+        return true;
+    }
+    else
+    {
+        cout << "Access Denied. Returning to Main page.\n";
+        return false;
+    }
 }
 
-string getCurrentDateTime() {
-    auto now = chrono::system_clock::now();
-    time_t currentTime = chrono::system_clock::to_time_t(now);
-    tm localTime;
-#ifdef _WIN32
-    localtime_s(&localTime, &currentTime); // Windows secure version
-#else
-    localtime_r(&currentTime, &localTime); // Linux/Unix
-#endif
-    char buffer[80];
-    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &localTime);
-    return string(buffer);
+// Get Current Time [year-month-date hour(24h):min:sec]
+string getTime()
+{
+    time_t now = time(0); // get current time from cpu
+    tm localTime;         // For human readable components
+    localtime_s(&localTime, &now);  // Windows safe version
+    char temp[80];
+    strftime(temp, sizeof(temp), "%Y-%m-%d %H:%M:%S", &localTime);
+    return string(temp);
 }
 
-void saveHistory(const string &X, const string &Y, int penalty, const string &alignedX, const string &alignedY) {
-    ofstream file(HISTORY_FILE, ios::app);
-    if (file.is_open()) {
-        file << "[" << getCurrentDateTime() << "]\n";  // Add timestamp
+// Save Alignment History
+void saveHistory(const string &X, const string &Y, int penalty, int Pgap, int Pxy, const string &alignedX, const string &alignedY)
+{
+    ofstream file(HISTORY_FILE, ios::app); // Append to file
+    if (file.is_open())
+    {
+        file << "[" << getTime() << "]\n";
         file << "String X: " << X << "\n";
         file << "String Y: " << Y << "\n";
+        file << "Gap Penalty: " << Pgap << "\n";
+        file << "Mismatch Penalty: " << Pxy << "\n";
         file << "Minimum Penalty: " << penalty << "\n";
         file << "Aligned X: " << alignedX << "\n";
         file << "Aligned Y: " << alignedY << "\n";
@@ -44,35 +67,44 @@ void saveHistory(const string &X, const string &Y, int penalty, const string &al
     }
 }
 
-void showHistory() {
-    
-    if (!checkPassword())
-        return;
+//Show Alignment History
+void showHistory()
+{
+
     ifstream file(HISTORY_FILE);
-    if (file.is_open()) {
+    if (file.is_open())
+    {
         string line;
         cout << "\n--- Alignment History ---\n";
-        while (getline(file, line)) {
+        while (getline(file, line))
             cout << line << endl;
-        }
-        cout << "--------------------------\n";
+        cout << "\n--------------------------\n";
         file.close();
-    } else {
+    }
+    else
+    {
         cout << "No history found.\n";
     }
 }
 
-void clearHistory() {
-    ofstream file(HISTORY_FILE, ios::trunc);
-    if (file.is_open()) {
+//Clear Alignment History
+void clearHistory()
+{
+    if (!checkPassword())
+        return;
+
+    ofstream file(HISTORY_FILE, ios::trunc); // Delete all content
+    if (file.is_open())
+    {
         cout << "History cleared successfully.\n";
         file.close();
     }
 }
 
-// ------------------- Menu Option Functions -------------------
-
-void performAlignment() {
+//  **Perform Global Sequence Alignment**
+void performAlignment()
+{
+    // 1. Input sequences and penalties
     string X, Y;
     int Pgap, Pxy;
     cout << "Enter string X: ";
@@ -86,86 +118,92 @@ void performAlignment() {
 
     int m = X.size();
     int n = Y.size();
+
+    // 2. Create DP table (2D dynamic array)
     vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
+    // 3. Initialize first row and column with gap penalties
     for (int i = 0; i <= m; i++)
         dp[i][0] = i * Pgap;
     for (int j = 0; j <= n; j++)
         dp[0][j] = j * Pgap;
 
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            int cost = (X[i - 1] == Y[j - 1]) ? 0 : Pxy;
-            dp[i][j] = min({
-                dp[i - 1][j - 1] + cost,
-                dp[i - 1][j] + Pgap,
-                dp[i][j - 1] + Pgap
-            });
+    // 4. Fill the DP table using Needleman-Wunsch algorithm
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            int cost;
+            if (X[i - 1] == Y[j - 1])
+                cost = 0;
+            else
+                cost = Pxy;
+
+            // Choose minimum among match/mismatch or inserting a gap
+
+            dp[i][j] = min({dp[i - 1][j - 1] + cost,  // diagonal (match/mismatch)
+                            dp[i - 1][j] + Pgap,      // up (gap in Y)
+                            dp[i][j - 1] + Pgap});    // left (gap in X)
         }
     }
 
+    // 5. Traceback to construct aligned sequences
     int i = m, j = n;
     string alignedX = "", alignedY = "";
-    while (i > 0 && j > 0) {
+    while (i > 0 && j > 0)
+    {
         int cost = (X[i - 1] == Y[j - 1]) ? 0 : Pxy;
-        if (dp[i][j] == dp[i - 1][j - 1] + cost) {
+
+        if (dp[i][j] == dp[i - 1][j - 1] + cost)
+        {
             alignedX = X[i - 1] + alignedX;
             alignedY = Y[j - 1] + alignedY;
-            i--; j--;
-        } else if (dp[i][j] == dp[i - 1][j] + Pgap) {
-            alignedX = X[i - 1] + alignedX;
-            alignedY = "-" + alignedY;
             i--;
-        } else {
-            alignedX = "-" + alignedX;
+            j--;
+        }
+        else if (dp[i][j] == dp[i - 1][j] + Pgap)
+        {
+            alignedX = X[i - 1] + alignedX;
+            alignedY = "_" + alignedY;
+            i--;
+        }
+        else
+        {
+            alignedX = "_" + alignedX;
             alignedY = Y[j - 1] + alignedY;
             j--;
         }
     }
-    while (i > 0) {
+
+    // Add remaining characters with gaps if one sequence is finished
+    while (i > 0)
+    {
         alignedX = X[i - 1] + alignedX;
-        alignedY = "-" + alignedY;
+        alignedY = "_" + alignedY;
         i--;
     }
-    while (j > 0) {
-        alignedX = "-" + alignedX;
+    while (j > 0)
+    {
+        alignedX = "_" + alignedX;
         alignedY = Y[j - 1] + alignedY;
         j--;
     }
 
+    // 6. Output the results
     cout << "\nMinimum Penalty: " << dp[m][n] << endl;
     cout << "Aligned X: " << alignedX << endl;
     cout << "Aligned Y: " << alignedY << endl;
 
-    saveHistory(X, Y, dp[m][n], alignedX, alignedY);
+    // 7. Save alignment history to file
+    saveHistory(X, Y, dp[m][n], Pgap, Pxy, alignedX, alignedY);
 }
 
-bool checkPassword() {
-    string savedPassword = readPasswordFromFile();
-    if (savedPassword.empty()) {
-        cout << "Password file missing or empty!\n";
-        return false;
-    }
-
-    string inputPassword;
-    cout << "Enter Admin Password: ";
-    cin >> inputPassword;
-
-    if (inputPassword == savedPassword) {
-        cout << "Access Granted!\n";
-        return true;
-    } else {
-        cout << "Access Denied. Exiting program.\n";
-        return false;
-    }
-}
-
-// ------------------- Main -------------------
-
-int main() {
-
-    while (true) {
-        cout << "\n--- Menu ---\n";
+// Main Menu
+int main()
+{
+    while (true)
+    {
+        cout << "\n--- Main Page ---\n";
         cout << "1. Perform Alignment\n";
         cout << "2. Show History\n";
         cout << "3. Clear History\n";
@@ -174,12 +212,22 @@ int main() {
         int choice;
         cin >> choice;
 
-        switch (choice) {
-            case 1: performAlignment(); break;
-            case 2: showHistory(); break;
-            case 3: clearHistory(); break;
-            case 4: cout << "Exiting...\n"; return 0;
-            default: cout << "Invalid choice. Try again.\n";
+        switch (choice)
+        {
+        case 1:
+            performAlignment();
+            break;
+        case 2:
+            showHistory();
+            break;
+        case 3:
+            clearHistory();
+            break;
+        case 4:
+            cout << "Exiting...\n";
+            return 0;
+        default:
+            cout << "Invalid choice. Try again.\n";
         }
     }
 }
